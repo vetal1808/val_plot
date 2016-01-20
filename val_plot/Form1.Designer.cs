e7613c0dfcf269ce6c1cf1f59fb201de83f97970
vetal1808 <vetal1808@gmail.com>
2016-01-20 17:23:45 +0300

    beta 0.1

---
+++ val_plot/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace val_plot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CanvasPanel = new System.Windows.Forms.Panel();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CanvasPanel
+            // 
+            this.CanvasPanel.Location = new System.Drawing.Point(12, 12);
+            this.CanvasPanel.Name = "CanvasPanel";
+            this.CanvasPanel.Size = new System.Drawing.Size(800, 650);
+            this.CanvasPanel.TabIndex = 0;
+            this.CanvasPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.CanvasPanel_Paint);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "pitch",
+            "roll",
+            "yaw",
+            "P_pitch",
+            "I_pitch",
+            "D_pitch",
+            "P_roll",
+            "I_roll",
+            "D_roll",
+            "P_yaw",
+            "I_yaw",
+            "D_yaw"});
+            this.checkedListBox1.Location = new System.Drawing.Point(827, 23);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(164, 184);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(826, 218);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(164, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Drawing";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(921, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 32);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "↑";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(881, 320);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(33, 33);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "←";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(921, 359);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(34, 31);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "↓";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(961, 320);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(35, 33);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "→";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(920, 320);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(35, 33);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "S";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(827, 282);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(32, 32);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "U";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(827, 320);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(32, 33);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "S";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.Control;
+            this.button9.Location = new System.Drawing.Point(827, 359);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(31, 31);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "D";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(879, 411);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(35, 33);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "I↑";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(921, 411);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(35, 33);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "D↑";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(840, 411);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(33, 33);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "P↑";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(878, 450);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(35, 33);
+            this.button13.TabIndex = 16;
+            this.button13.Text = "I↓";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(920, 450);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(35, 33);
+            this.button14.TabIndex = 15;
+            this.button14.Text = "D↓";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(839, 450);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(33, 33);
+            this.button15.TabIndex = 14;
+            this.button15.Text = "P↓";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.Location = new System.Drawing.Point(818, 256);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 20);
+            this.textBox1.TabIndex = 17;
+            this.textBox1.Text = "POWER";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(0, 0);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 18;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(0, 0);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 19;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(898, 256);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(81, 20);
+            this.textBox4.TabIndex = 20;
+            this.textBox4.Text = "Normal vector";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 650);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.CanvasPanel);
+            this.Name = "Form1";
+            this.Text = "Draw";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Panel CanvasPanel;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ val_plot/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace val_plot
+{
+    public partial class Form1 : Form
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new Form1());
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            CanvasPanel.Invalidate();
+            this.Size = new Size(1000, 650);
+        }
+
+        #region [Properties]
+
+        static int w = 800, h = 650, div2 = 65536 / h;
+        // w, h - размеры окна div2 - спец. коэф, позволяет разложить весь дипазон 2байтного инта на всю высоту окна
+
+
+        static bool _continue = true;
+        static bool _draw = true;
+        static SerialPort _serialPort;
+        static int[] prev = new int[13];
+        static int[] curr = new int[13];
+        static Pen pRed = new Pen(Color.Red, 1);
+        static Pen pGreen = new Pen(Color.Green, 1);
+        static Pen pBlue = new Pen(Color.Blue, 1);
+        static Pen pBlack = new Pen(Color.Black, 1);
+        static Pen pGray = new Pen(Color.Gray, 1);
+        static int j = 0;
+        static bool[] _isDrawing = new bool[12];
+        static Graphics g;
+
+        #endregion [Properties]
+
+        #region [Handlers]
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _serialPort.Close();
+            _continue = false;
+        }
+
+        private void CanvasPanel_Paint(object sender, PaintEventArgs e)
+        {
+            g = this.CanvasPanel.CreateGraphics();
+            Thread drawerThread = new Thread(Drawer);
+            drawerThread.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _draw = !_draw;
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var itemIndex = ((sender as ListBox).SelectedIndex);
+            _isDrawing[itemIndex] = !_isDrawing[itemIndex];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _serialPort.Write("w");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            _serialPort.Write("q");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _serialPort.Write("a");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            _serialPort.Write("d");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            _serialPort.Write("s");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            _serialPort.Write("U");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            _serialPort.Write("Q");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            _serialPort.Write("D");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            _serialPort.Write("B");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            _serialPort.Write("N");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            _serialPort.Write("M");
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            _serialPort.Write("b");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            _serialPort.Write("n");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            _serialPort.Write("m");
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            _serialPort = new SerialPort
+            {
+                PortName = "COM10",
+                BaudRate = 9600,
+                ReadTimeout = 500,
+                WriteTimeout = 500
+            };
+
+            
+            _serialPort.Open();
+
+            _continue = true;
+
+
+            
+        }
+
+#endregion [Handlers]
+
+        public static void Drawer()
+        {
+            string message;
+            g.DrawLine(pGray, j, h / 2, w, h / 2);
+            while (_continue)
+            {
+                try
+                {
+                    message = _serialPort.ReadLine();
+                    if(message[1] == ':' && message[0] >='A' && message[0]<='W')
+                    {
+                        char lol = message[0];
+                        message = message.Remove(0, 2);
+                        switch (lol)
+                        {
+                            case 'A':                            
+                                curr[0] = (Convert.ToInt32(message) + 32768) / div2;
+                                break;
+                            case 'B':
+                                curr[1] = (Convert.ToInt32(message) + 32768) / div2;
+                                break;
+                            case 'C':
+                                curr[2] = (Convert.ToInt32(message) + 32768) / div2;
+                                break;
+                            case 'D':
+                                curr[3] = (Convert.ToInt32(message) + 32768) / div2;
+                                break;
+                            case 'E':
+                                curr[4] = (Convert.ToInt32(message) + 32768) / div2;
+                                break;
+                            case 'F':
+                                curr[5] = (Convert.ToInt32(message) + 32768) / div2;
+                                break;
+                            case 'G':
+                                curr[6] = (Convert.ToInt32(message) + 32768) / div2;
+                                break;
+                            case 'H':
+                                curr[7] = (Convert.ToInt32(message) + 32768) / div2;
+                                break;
+                            case 'I':
+                                curr[8] = (Convert.ToInt32(message) + 32768) / div2;
+                                break;
+                            case 'J':
+                                curr[9] = (Convert.ToInt32(message) + 32768) / div2;
+                                break;
+                            case 'K':
+                                curr[10] = (Convert.ToInt32(message) + 32768) / div2;
+                                break;
+                            case 'L':
+                                curr[11] = (Convert.ToInt32(message) + 32768) / div2;
+                                break;
+                            case 'W':
+                                if (_draw)
+                                {
+                                    for(var k=0; k<12; k+=3)
+                                    {
+                                        if (_isDrawing[0+k])
+                                            g.DrawLine(pRed, j, prev[0 + k], j + 1, curr[0 + k]);
+                                        if (_isDrawing[1 + k])
+                                            g.DrawLine(pGreen, j, prev[1 + k], j + 1, curr[1 + k]);
+                                        if (_isDrawing[2 + k])
+                                            g.DrawLine(pBlue, j, prev[2 + k], j + 1, curr[2 + k]);
+                                    }
+                                    j = (j + 1) % w;
+                                    for (var q = 0; q < 12; q++)
+                                        prev[q] = curr[q];
+                                }
+                                break;
+                        }
+
+                    }
+                }
+                catch (Exception) { }
+                if (j < 1)
+                {
+                    try
+                    {
+                        g.Clear(Color.White);
+                        g.DrawLine(pGray, j, h / 2, w, h / 2); // середина экрана
+                    }
+                    catch (Exception) { }
+                }
+            }
+        }
+    }
+}
